164899df68ec7f1af29fdaeedacc4083af5c36d1
DataScienceWiz <wolesword@gmail.com>
2020-12-15 00:46:59 -0500

    New file

--- Intro.py
+++ Intro.py
@@ -28,8 +28,8 @@ html_temp = """
     <h3 style="color:#666666;text-align:left; line-height: 1.5">
     <p>This Web App will predict student mathematics performance 
     in exams once the following (6) parameters are inputed.<br> 
-    This is based on Deep learning algorithms with data from a School in North America.</p></h3>
-    Dataset: <br> https://www.kaggle.com/spscientist/students-performance-in-exams<br> 
+    This is based on Deep learning algorithms with data from a School in North America.
+    Dataset: <br> https://www.kaggle.com/spscientist/students-performance-in-exams</p></h3>
     </div>
     """
 st.markdown(html_temp, unsafe_allow_html=True)
